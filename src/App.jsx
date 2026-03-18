c771b9b46a3cd83ab49f5096ce77585247978727
Uzair Ahmed <uzair.a.ahmed03@gmail.com>
2026-03-17 20:31:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div>
+      <h1>PickupPal</h1>
+      <p>Find and join pickup games on campus!</p>
+
+      <h2>Upcoming Games</h2>
+      <ul>
+        <li>Basketball @ Nick - 5pm</li>
+        <li>Soccer @ Near West - 6pm</li>
+        <li>Volleyball @ Bakke - 7pm</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
